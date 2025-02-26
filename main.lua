75cd52172d4dbe7688df6e5d4f377a3bb47382b6
Drumstepp <l4pierce+drumstepp@gmail.com>
2025-02-25 19:23:15 -0500

    add new sound

--- main.lua
+++ main.lua
@@ -1,2 +1,3 @@
 SharedMedia = LibStub("LibSharedMedia-3.0")
 SharedMedia:Register("sound", "Foot Woman Reverb", [[Interface\Addons\DKGSoundPack\Sounds\foot_woman_reverb.mp3]])
+SharedMedia:Register("sound", "Mommy Toes",  [[Interface\Addons\DKGSoundPack\Sounds\mommy_toes.mp3]])
